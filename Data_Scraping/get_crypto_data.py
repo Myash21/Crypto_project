4102a7f8e278d846d8ecb378830124d033837da4
Yash Mhaskar <myash100@gmail.com>
2024-10-28 09:32:56 -0700

    minor fixes

--- Data_Scraping/get_crypto_data.py
+++ Data_Scraping/get_crypto_data.py
@@ -1,12 +1,6 @@
 import requests
 import pandas as pd
 from datetime import datetime
-import os
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, r2_score
-import joblib
-
 
 def fetch_crypto_data(crypto_pair, start_date):
     """
